5c646c300de94433c647e980c577054babade2b4
Chidambaran Subramanian <chiddu@aurospaces.com>
2015-09-08 15:02:28 +0530

    Adding a new file for generating all

---
+++ genall.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('util.php');
+
+$ini_list = parse_ini_file($argv[1]);
+
+$dbh = new PDO('mysql:host=localhost;dbname='.$ini_list['dbname'], $ini_list['dbuser'],$ini_list['dbpwd'], array( PDO::ATTR_PERSISTENT => false));
+
+d4($dbh);
+#$query = "desc ".$argv[2];
+#d4($query);
+$stmt = $dbh->query("show tables");
+
+
+$rowlist = $stmt->fetchAll();
+d3($rowlist);
+
